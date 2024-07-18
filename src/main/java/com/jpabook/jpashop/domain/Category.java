26a8fca03c3f5e3d5146b4d3c32ce22ae943d4af
yeongwoo <jyy0223@naver.com>
2024-07-18 18:29:33 +0900

    [feat] entity 추가: Member, Order, OrderItem, OrderStatus(enum), Delivery, DeliveryStatus(enum), Address(value type), Category, Item(Album, Book, Movie)

---
+++ src/main/java/com/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,42 @@
+package com.jpabook.jpashop.domain;
+
+
+import com.jpabook.jpashop.domain.item.Item;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    // 셀프로 양방향 연관관계: 카테고리 안에 카테고리를 넣는 것
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
+}
