9c85b656ad377a88eb6460f1a3ba221f72eab423
yeongwoo <jyy0223@naver.com>
2024-07-20 01:48:20 +0900

    [feat] 전체 주문 가격 조회 로직 추가

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -80,4 +80,16 @@ public class Order {
             orderItem.cancel();
         }
     }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- src/main/java/com/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/com/jpabook/jpashop/domain/OrderItem.java
@@ -28,4 +28,9 @@ public class OrderItem {
     public void cancel() {
         getItem().addStock(count);
     }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
