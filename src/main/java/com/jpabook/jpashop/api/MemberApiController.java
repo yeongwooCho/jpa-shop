724988275b61c5ce10e56ac1e4a21b622618bcaa
yeongwoo <jyy0223@naver.com>
2024-08-08 00:29:51 +0900

    [feat] 회원수정 API 구현

--- src/main/java/com/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/com/jpabook/jpashop/api/MemberApiController.java
@@ -4,10 +4,10 @@ package com.jpabook.jpashop.api;
 import com.jpabook.jpashop.api.dto.CreateMemberRequest;
 import com.jpabook.jpashop.api.dto.CreateMemberResponse;
 import com.jpabook.jpashop.api.dto.UpdateMemberRequest;
+import com.jpabook.jpashop.api.dto.UpdateMemberResponse;
 import com.jpabook.jpashop.domain.Member;
 import com.jpabook.jpashop.service.MemberService;
 import jakarta.validation.Valid;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +29,16 @@ public class MemberApiController {
         member.setName(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
 }
